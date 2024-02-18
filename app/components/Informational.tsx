fbaf986c941ab191f9744a8cb80062fb9b6b987d
Chaffexd <shanechaffe@gmail.com>
2024-02-18 13:38:33 +0000

    added info section

--- app/components/Informational.tsx
+++ app/components/Informational.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 type InformationProps = {
   informationalData: {
     title: string;
@@ -19,8 +21,24 @@ type InformationProps = {
 
 const Informational = ({ informationalData }: InformationProps) => {
   console.log("INFO = ", informationalData)
+  const { title, description } = informationalData;
+  const { items } = informationalData.cardsCollection;
   return (
-    <div>Informational</div>
+    <section className="w-full text-center bg-gray-800 rounded-lg h-9/12">
+      <h4 className="text-white text-4xl pt-12">{title}</h4>
+      <p className="text-gray-300 w-1/2 m-auto mt-8 mb-16">{description}</p>
+      <div className="w-full">
+        <ul className="flex justify-between w-10/12 m-auto gap-8">
+          {items.map((item) => (
+            <li key={item.title} className="flex flex-col items-center bg-emerald-700 rounded-lg w-80 h-72 justify-center -mb-28 p-4">
+              <Image src={item.image.image.url} alt={item.image.altText} width={80} height={80} className="mb-8" />
+              <h5 className="font-bold text-gray-300 mb-2">{item.title}</h5>
+              <p className="text-gray-300">{item.description}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
   )
 }
 
